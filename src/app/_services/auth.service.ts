ff74db1bf56558233d052fed6f05d34a6e4a345c
Denis Sudeev <denissudeev@gmail.com>
2020-03-01 21:00:15 +0300

    Added client login and register

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { AuthUser } from '../_models/authUser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:5000/api/auth/';
+  userToken: any;
+
+constructor(private http: HttpClient) { }
+
+login(model: any) {
+  return this.http.post<AuthUser>(this.baseUrl +  'login', model, {headers: new HttpHeaders()
+    .set('Content-type', 'application/json')})
+    .pipe(map(user => {
+        if (user) {
+          localStorage.setItem('token', user.tokenString);
+          this.userToken = user.tokenString;
+        }
+    }));
+}
+
+register(model: any) {
+  return this.http.post(this.baseUrl + 'register', model, {headers: new HttpHeaders()
+    .set('Content-type', 'application/json')});
+}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
-],
-   providers: [],
+      HttpClientModule,
+      FormsModule
+   ],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
